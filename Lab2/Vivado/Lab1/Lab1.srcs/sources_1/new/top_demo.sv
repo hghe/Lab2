6cd41aee431636da292c1a82c9f961c3ff8cd0d8
Cornelius <gabriel.j.cornelius@okstate.edu>
2023-10-20 16:35:37 -0500

    started on vivado

--- Lab2/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
+++ Lab2/Vivado/Lab1/Lab1.srcs/sources_1/new/top_demo.sv
@@ -46,14 +46,47 @@ module top_demo
   
   // Place TicTacToe instantiation here
   
+//DES des (key, plaintext, encrypt, cbc, iv, ciphertext);
+logic [15:0] toSeg;
+
+logic cbc;
+logic encrypt;
+logic [63:0] iv;
+logic [63:0] key;
+logic [63:0] plaintext;
+logic [63:0] ciphertext;
+
+
+/*
+
+assign [0]cbc = [193] tv;
+assign [0]encrypt = [192] tv;
+assign [63:0] plaintext = [191:128] tv;
+assign [63:0] key = [127:64] tv;
+assign [63:0] iv = [63:0] tv;
+*/
+
+assign plaintext = 64'hed7bc587a26f8c67;
+assign key = 64'h3b3898371520f75e;
+assign iv = 64'h0000000000000000;
+
+assign cbc = 1'b0;
+assign encrypt = 1'b1;
+
+
+DES des(key, plaintext, encrypt, cbc, iv, ciphertext);
+
+
+
+
   // 7-segment display
   segment_driver driver(
   .clk(smol_clk),
   .rst(btn[3]),
-  .digit0(sw[3:0]),
-  .digit1(4'b0111),
-  .digit2(sw[7:4]),
-  .digit3(4'b1111),
+  .digit0(toSeg[3:0]),
+  .digit1(toSeg[7:4]),
+  .digit2(toSeg[11:8]),
+  .digit3(toSeg[15:12]),
   .decimals({1'b0, btn[2:0]}),
   .segment_cathodes({sseg_dp, sseg_cg, sseg_cf, sseg_ce, sseg_cd, sseg_cc, sseg_cb, sseg_ca}),
   .digit_anodes(sseg_an)
@@ -62,10 +95,42 @@ module top_demo
 // Register logic storing clock counts
   always@(posedge sysclk_125mhz)
   begin
-    if(btn[3])
-      CURRENT_COUNT = 17'h00000;
-    else
-      CURRENT_COUNT = NEXT_COUNT;
+    case(sw[3:0])
+      4'b0000: toSeg = ciphertext[63:48];
+      4'b0001: toSeg = ciphertext[47:32];
+      4'b0010: toSeg = ciphertext[31:16];
+      4'b0011: toSeg = ciphertext[15:0];
+      4'b0100: toSeg = key[63:48];
+      4'b0101: toSeg = key[47:32];
+      4'b0110: toSeg = key[31:16];
+      4'b0111: toSeg = key[15:0];
+      4'b1000: toSeg = plaintext[63:48];
+      4'b1001: toSeg = plaintext[47:32];
+      4'b1010: toSeg = plaintext[31:16];
+      4'b1011: toSeg = plaintext[15:0];
+
+    endcase
+  /*
+    case(sw[5:4])
+      2'b00: 
+      begin
+        tv = 194'h0_1_ed7bc587a26f8c67_3b3898371520f75e_0000000000000000;
+      end
+      2'b01:
+      begin
+        tv = 194'h0_1_ed7bc587a26f8c67_3b3898371520f75e_0000000000000000;
+      end
+      2'b10:
+      begin
+        tv = 194'h0_1_ed7bc587a26f8c67_3b3898371520f75e_0000000000000000;
+      end
+      2'b11:
+      begin
+        tv = 194'h0_1_ed7bc587a26f8c67_3b3898371520f75e_0000000000000000;
+      end
+
+    endcase
+  */
   end
   
   // Increment logic
